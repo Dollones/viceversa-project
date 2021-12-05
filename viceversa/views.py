c7fec51413e7006dc59e30242f6e9fbcf63e3d76
unknown <danyasven@gmail.com>
2021-12-05 21:21:16 +0300

    Added words count feature

--- viceversa/views.py
+++ viceversa/views.py
@@ -8,7 +8,9 @@ def home(request):
 def reverse(request):
     original_message = request.GET['message']
     reversed_message = original_message[::-1]
+    words_amount = len(original_message.split())
     return render(request,
                   'reverse.html',
-                  {'original_message': original_message, 'reversed_message': reversed_message}
+                  {'original_message': original_message, 'reversed_message': reversed_message,
+                   'words_amount': words_amount}
                   )
